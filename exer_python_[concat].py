7a4989c583cc3168f2a62c020b945696b9feda53
Roberto Ruivo <robertoruivo@ua.pt>
2024-01-10 21:37:07 +0000

    Concatenacao

---
+++ exer_python_[concat].py
@@ -0,0 +1,4 @@
+print("Bem-Vindo!")
+street = input("Qual o seu Nome e onde nasceu?\n")
+pet = input("Qual o nome do seu animal de estimação?\n")
+print("A tua Banda chama-se " + street + " " + pet)
